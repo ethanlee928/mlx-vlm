26e01abd12315c46169788db1e9f566c2fe984f3
ethanlee <ethanlee@sebit.world>
2026-01-28 17:10:55 +0800

    fix AttributeError: 'NoneType' object has no attribute 'offset'

--- mlx_vlm/models/qwen2_5_vl/language.py
+++ mlx_vlm/models/qwen2_5_vl/language.py
@@ -152,8 +152,9 @@ class Attention(nn.Module):
         kv_seq_len = keys.shape[-2]
 
         if position_ids is None:
-            kv_seq_len += cache.offset + 1
-            position_ids = mx.arange(cache.offset, cache.offset + L)
+            cache_offset = cache.offset if cache is not None else 0
+            kv_seq_len += cache_offset + 1
+            position_ids = mx.arange(cache_offset, cache_offset + L)
             position_ids = mx.expand_dims(position_ids, axis=0)
             position_ids = mx.tile(position_ids, (3, 1, 1))
         else:
